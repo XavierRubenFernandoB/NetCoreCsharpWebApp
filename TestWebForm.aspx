3fb553626d8b34c44785e1527ae3993dbe1330f2
Xavier Fernando <xavier.r.fernando@hotmail.com>
2020-05-26 19:13:39 +0530

    dictionary sample

--- TestWebForm.aspx
+++ TestWebForm.aspx
@@ -9,6 +9,27 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <table style="border:1px solid black;">
+                <tr>
+                    <td>Code</td>
+                    <td>
+                        <asp:TextBox ID="txtCode" runat="server" AutoPostBack="True" OnTextChanged="txtCode_TextChanged"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Capital</td>
+                    <td>
+                        <asp:TextBox ID="txtCapital" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblMsg" runat="server"></asp:Label></td>
+                </tr>
+            </table>
         </div>
     </form>
 </body>
--- TestWebForm.aspx.cs
+++ TestWebForm.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -15,11 +16,54 @@ namespace NetCoreCsharpWebApp
 
             Response.Write("Partial Class<br>");
             Response.Write("-----------------------------------------<br>");
-
             PartialClassCustomer obj = new PartialClassCustomer();
             Response.Write(obj.Print());
+            //Response.Write(MyFormPrint());
+            Response.Write("<br><br>");
 
-            Response.Write(MyFormPrint());
+            Response.Write("Dictionary<br>");
+            Response.Write("-----------------------------------------<br>");
+
+            if (Session["sDictCountries"]  == null)
+            {
+                Country country1 = new Country() { Code = "IND", Name = "India", Capital = "New Delhi" };
+                Country country2 = new Country() { Code = "USA", Name = "United States", Capital = "New York" };
+                Country country3 = new Country() { Code = "CAN", Name = "Canada", Capital = "Ottawa" };
+
+                Dictionary<string, Country> dictcountry = new Dictionary<string, Country>();
+                dictcountry.Add(country1.Code, country1);
+                dictcountry.Add(country2.Code, country2);
+                dictcountry.Add(country3.Code, country3);
+
+                Session["sDictCountries"] = dictcountry;
+            }
         }
+
+        protected void txtCode_TextChanged(object sender, EventArgs e)
+        {
+            Dictionary<string, Country> sdictCtry = (Dictionary<string, Country>)Session["sDictCountries"];
+
+            Country resultCountry = sdictCtry.ContainsKey(txtCode.Text.ToUpper()) ? sdictCtry[txtCode.Text.ToUpper()] : null;
+
+            if (resultCountry != null)
+            {
+                txtName.Text = resultCountry.Name;
+                txtCapital.Text = resultCountry.Capital;
+                lblMsg.Text = "";
+            }
+            else
+            {
+                txtName.Text = "";
+                txtCapital.Text = "";
+                lblMsg.Text = "Invalid Code";
+            }
+        }
+    }
+
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string  Capital { get; set; }
     }
 }
